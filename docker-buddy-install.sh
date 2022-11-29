3d275ac76e55c2a2451ca261ebf2e903cd105ef8
RubendAvidaPereira <rubenpereira2009@live.com.pt>
2022-11-29 13:07:02 +0000

    Update 1.0.4

--- docker-buddy-install.sh
+++ docker-buddy-install.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 # Author: Ruben Pereira
-# Version: 1.0.3
+# Version: 1.0.4
 
 # This file is a simple script for the docker-buddy installation
+
+# 1. Set the file to write to
+user=$(whoami)
+file="/home/$user/.bashrc"
+
+# 2. Append multiple lines with '>>' and 'cat', EOT represents end of file
+cat << EOT >> $file
+if [ -f ~/docker-buddy/.docker-buddy ]; then
+   source ~/docker-buddy/.docker-buddy
+fi
+
+if [ -f ~/docker-buddy/.docker-buddy-usage ]; then
+   source ~/docker-buddy/.docker-buddy-usage
+fi
+EOT
+
+exec bash
